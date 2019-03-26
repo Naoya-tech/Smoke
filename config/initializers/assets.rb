577109c5fc0fbb500ad7e0fdef8e68a5c19c2a7c
Naoya-tech <naoya.techwork@gmail.com>
2019-03-26 13:15:17 +0900

    herokuデプロイのための修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 # Rails.application.config.assets.precompile += %w( *.eot *.woff *.ttf *.svg *.otf *.png *.jpg *.jpeg *.gif vendor.css vendor.js )
+
+Rails.application.config.assets.initialize_on_precompile = false
