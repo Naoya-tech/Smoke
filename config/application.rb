c0fd564fdc1225debc26e7c1310e5b0864a57afa
Naoya-tech <naoya.techwork@gmail.com>
2019-03-26 11:39:28 +0900

    herokuデプロイのための修正

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 module Smoke
   class Application < Rails::Application
