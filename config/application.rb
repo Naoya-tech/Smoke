6c725fc17d8476d950cf1b6ae1abec7bdc9ffba6
Naoya-tech <naoya.techwork@gmail.com>
2019-03-26 11:42:33 +0900

    herokuデプロイのための修正

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,6 +12,7 @@ module Smoke
     config.time_zone = 'Tokyo' # タイムゾーンもついでに変更
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
