59d552adc61e40d65d77f4ac83318f09e8b9aad9
Naoya-tech <naoya.techwork@gmail.com>
2019-03-26 11:36:55 +0900

    herokuデプロイのための修正

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module Smoke
   class Application < Rails::Application
@@ -17,6 +18,5 @@ module Smoke
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.initialize_on_precompile = false
   end
 end
